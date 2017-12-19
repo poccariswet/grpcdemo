66b26167c69629e3f51d329ad2031ad928a32f2c
soeyusuke <s1240231@gmail.com>
2017-12-19 21:26:19 +0900

    [FIX] fixed to method

--- client/main.go
+++ client/main.go
@@ -17,74 +17,76 @@ const (
 )
 
 var (
+	data []string
+)
+
+type BookInfo struct {
 	id     string
 	title  string
 	author string
 	isbn13 string
 	state  string
 	pic    string
-	data   []string
-)
+}
 
 func main() {
 	var mode string
+	s := BookInfo{}
 	flag.StringVar(&mode, "mode", "", "[fetch|add|list|update|delete]を指定")
-	flag.StringVar(&id, "id", "", "idを指定")
-	flag.StringVar(&title, "title", "", "titleを指定")
-	flag.StringVar(&author, "author", "", "authorを指定")
-	flag.StringVar(&isbn13, "isbn13", "", "isbn13を指定| ture or false")
-	flag.StringVar(&state, "state", "", "stateを指定")
-	flag.StringVar(&pic, "pic", "", "picを指定")
+	flag.StringVar(&s.id, "id", "", "idを指定")
+	flag.StringVar(&s.title, "title", "", "titleを指定")
+	flag.StringVar(&s.author, "author", "", "authorを指定")
+	flag.StringVar(&s.isbn13, "isbn13", "", "isbn13を指定| ture or false")
+	flag.StringVar(&s.state, "state", "", "stateを指定")
+	flag.StringVar(&s.pic, "pic", "", "picを指定")
 	flag.Parse()
+
 	conn, err := grpc.Dial("localhost:"+port, grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	defer conn.Close()
+
 	c := pb.NewServiceClient(conn)
 	ctx := context.Background()
 
 	switch mode {
 	case "fetch":
-		if id == "" {
+		if s.id == "" {
 			log.Fatalf("not set id")
 		}
-		if err := fetch(ctx, &c, id); err != nil {
+		if err := s.fetch(ctx, &c); err != nil {
 			log.Fatal(err)
 		}
 
 	case "add":
-		if title == "" || author == "" || isbn13 == "" || state == "" || pic == "" {
+		if s.title == "" || s.author == "" || s.isbn13 == "" || s.state == "" || s.pic == "" {
 			log.Fatalf("no set params are title, author, isbn13, state, pic")
-		} else {
-			AppendData()
 		}
-		if err := add(ctx, &c, data); err != nil {
+		if err := s.add(ctx, &c); err != nil {
 			log.Fatal(err)
 		}
 		fmt.Println("success add the params")
 
 	case "list":
-		if err := list(ctx, &c); err != nil {
+		if err := s.list(ctx, &c); err != nil {
 			log.Fatal(err)
 		}
 
 	case "update":
-		if id == "" || title == "" || author == "" || isbn13 == "" || state == "" || pic == "" {
+		if s.id == "" || s.title == "" || s.author == "" || s.isbn13 == "" || s.state == "" || s.pic == "" {
 			log.Fatalf("no set params are id, title, author, isbn13, state, pic")
-		} else {
-			AppendData()
 		}
-		if err := update(ctx, &c, data); err != nil {
+		if err := s.update(ctx, &c); err != nil {
 			log.Fatal(err)
 		}
 		fmt.Println("success update the params")
 
 	case "delete":
-		if id == "" {
+		if s.id == "" {
 			log.Fatalf("not set id")
 		}
-		if err := delete(ctx, &c, id); err != nil {
+		if err := s.delete(ctx, &c); err != nil {
 			log.Fatal(err)
 		}
 		fmt.Println("delete success")
@@ -93,9 +95,9 @@ func main() {
 	}
 }
 
-func fetch(ctx context.Context, c *pb.ServiceClient, fid string) error {
+func (s BookInfo) fetch(ctx context.Context, c *pb.ServiceClient) error {
 	res, err := (*c).Fetch(ctx, &pb.FetchRequest{
-		Id: fid,
+		Id: s.id,
 	})
 	if err != nil {
 		return err
@@ -116,9 +118,9 @@ func fetch(ctx context.Context, c *pb.ServiceClient, fid string) error {
 	return nil
 }
 
-func add(ctx context.Context, c *pb.ServiceClient, data []string) error {
+func (s BookInfo) add(ctx context.Context, c *pb.ServiceClient) error {
 	pbbook := pb.Book{}
-	if err := arraydata(&pbbook, data); err != nil {
+	if err := arraydata(&pbbook, s); err != nil {
 		return err
 	}
 
@@ -129,7 +131,7 @@ func add(ctx context.Context, c *pb.ServiceClient, data []string) error {
 	return nil
 }
 
-func list(ctx context.Context, c *pb.ServiceClient) error {
+func (s BookInfo) list(ctx context.Context, c *pb.ServiceClient) error {
 	res, err := (*c).ListAll(ctx, &pb.Empty{})
 	if err != nil {
 		return err
@@ -146,9 +148,9 @@ func list(ctx context.Context, c *pb.ServiceClient) error {
 	return nil
 }
 
-func update(ctx context.Context, c *pb.ServiceClient, data []string) error {
+func (s BookInfo) update(ctx context.Context, c *pb.ServiceClient) error {
 	pbbook := pb.Book{}
-	if err := arraydata(&pbbook, data); err != nil {
+	if err := arraydata(&pbbook, s); err != nil {
 		return err
 	}
 
@@ -159,9 +161,9 @@ func update(ctx context.Context, c *pb.ServiceClient, data []string) error {
 	return nil
 }
 
-func delete(ctx context.Context, c *pb.ServiceClient, did string) error {
+func (s BookInfo) delete(ctx context.Context, c *pb.ServiceClient) error {
 	_, err := (*c).Delete(ctx, &pb.DeleteRequest{
-		Id: did,
+		Id: s.id,
 	})
 	if err != nil {
 		return err
@@ -169,33 +171,25 @@ func delete(ctx context.Context, c *pb.ServiceClient, did string) error {
 	return nil
 }
 
-func arraydata(pbbook *pb.Book, data []string) error {
-	var judg bool
-	if data == nil {
-		return fmt.Errorf("data's contents is nothing")
-	}
-	if data[4] == "true" {
-		judg = true
-	} else {
-		judg = false
-	}
-
+func arraydata(pbbook *pb.Book, b BookInfo) error {
 	*pbbook = pb.Book{
-		Id:     data[0],
-		Title:  data[1],
-		Author: data[2],
-		Isbn13: data[3],
-		State:  judg,
-		Pic:    data[5],
+		Id:     b.id,
+		Title:  b.title,
+		Author: b.author,
+		Isbn13: b.isbn13,
+		State:  confilmState(b.state),
+		Pic:    b.pic,
 	}
 	return nil
 }
 
-func AppendData() {
-	data = append(data, id)
-	data = append(data, title)
-	data = append(data, author)
-	data = append(data, isbn13)
-	data = append(data, state)
-	data = append(data, pic)
+func confilmState(j string) bool {
+	var judg bool
+	if j == "true" {
+		judg = true
+		return judg
+	} else {
+		judg = false
+		return judg
+	}
 }
